6085b2dc55cc4439c6875c733439c719b9a815e4
Hans <hanshanglin2017@gmail.com>
2019-04-08 00:49:50 +0800

    NN build up

--- BPNN.py
+++ BPNN.py
@@ -2,22 +2,27 @@ import torch
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
-
+import numpy as np
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 # check gpu
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-class BPNN(nn.Module):
-    def __init__(self,input_size,hidden_size):
-        super(BPNN,self).__init__()
-
-        # set sigmoid function as transfer function
-        self.tfc = nn.Sigmoid()
-
-
 
 if __name__ == '__main__':
-    print(torch.__version__)
+    #print(torch.__version__)
+    temp_x = np.loadtxt("qsort_qsort.py.csv",dtype=np.float32, delimiter=",")
+    temp_y = np.loadtxt("qsort_result.txt",dtype=np.float32, delimiter=",")
+    print(type(temp_x[0][0]))
+    x_train, x_test, y_train, y_test = train_test_split(temp_x, temp_y, test_size=0.25, random_state=0)
+
+    n_in, n_h, n_out, batch_size = len(x_train[0]), 3, 1, len(x_train)
+
+    x_train = torch.tensor(x_train)
+    y_train = torch.tensor(y_train.T)
+
+    '''
     n_in ,n_h, n_out, batch_size = 9,3,1,7
     x = torch.tensor([[1.,1.,1.,1.,0.,1.,0.,0.,1.],
                      [1.,0.,0.,0.,1.,1.,1.,1.,0],
@@ -33,28 +38,30 @@ if __name__ == '__main__':
                       [0.],
                       [1.],
                       [1.]])
-    print(x)
-    print(y)
+    
+    '''
+    #print(x_train)
+    #print(y_train)
+
     model = nn.Sequential(nn.Linear(n_in,n_h),
                           nn.Sigmoid(),
                           nn.Linear(n_h,n_out),
                           nn.Sigmoid())
-    print(model)
+    #print(model)
     # loss
     criterion = torch.nn.MSELoss()
-    print(criterion)
+    #print(criterion)
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-    print(optimizer)
-
-    for i in range(50):
+    #print(optimizer)
+    loss_list =[]
+    for i in range(5000):
         # forward prapagation
-        y_pred = model(x)
+        y_pred = model(x_train)
 
         # loss
-        loss = criterion(y_pred,y)
-        print('i: ', i, ' loss: ', loss.item())
+        loss = criterion(y_pred,y_train)
         #print(y_pred)
-
+        loss_list.append(float(loss))
 
         #print(model.named_parameters())
 
@@ -63,8 +70,19 @@ if __name__ == '__main__':
         #print(model.state_dict())
         optimizer.step()
         #
-        '''params = list(model.named_parameters())
-        for (name, param) in params:
-            print(name, param.grad)'''
+        if i%100 == 0:
+            print('i: ', i, ' loss: ', loss.item())
+            #params = list(model.named_parameters())
+            #for (name, param) in params:
+            #    print(name, param.grad)
         #print("\n-----------------------------\n")
+
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    plt.xlabel('step')
+    plt.ylabel('loss')
+    ax1.scatter([i for i in range(5000)],loss_list,c = 'b',marker = '.')
+    plt.show()
+
 
